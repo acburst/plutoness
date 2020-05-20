3103f3cbb57135fd20fdc785631acdade4f72324
Alan Chang <alanchan@Alans-MacBook-Pro.local>
2020-05-19 20:00:10 -0700

    time sync

--- psychiccontinuity/js/init.js
+++ psychiccontinuity/js/init.js
@@ -46,11 +46,37 @@ const videoOptions = '?controls=1&modestbranding=1&autohide=1&autoplay=1&enablej
 const videos = [{
   // 10:20 PM - 2:20 AM
   url: 'https://www.youtube.com/embed/hABRwKHU1OE' + videoOptions,
-  length: 7 * 60 * 60 // 7 hours in seconds
+  length: 4 * 60 * 60 // 7 hours in seconds
 }, {
-  // 5:@0 PM - 6:20 PM
+  // 2:20 AM - 5:20 AM
+  url: 'https://www.youtube.com/embed/mNwGOJEGSeE' + videoOptions,
+  length: 3 * 60 * 60 // 3 hours in seconds
+}, {
+  // 5:20 AM - 9:20 AM
+  url: 'https://www.youtube.com/embed/5iPX9Fmugxw' + videoOptions,
+  length: 4 * 60 * 60 // 4 hours in seconds
+}, {
+  // 9:20 AM - 12:20 PM
+  url: 'https://www.youtube.com/embed/NQwWj1xPE4E' + videoOptions,
+  length: 3 * 60 * 60 // 3 hours in seconds
+}, {
+  // 12:20 PM - 4:20 PM
+  // TODO: add real video
+  url: 'https://www.youtube.com/embed/lM02vNMRRB0' + videoOptions,
+  length: 4 * 60 * 60 // 4 hours in seconds
+}, {
+  // 4:20 PM - 5:20 PM
+  url: 'https://www.youtube.com/embed/iE1BjatrdCI' + videoOptions,
+  length: 60 * 60 // 1 hour in seconds
+}, {
+  // 5:20 PM - 6:20 PM
   url: 'https://www.youtube.com/embed/WojKvsFyD7A' + videoOptions,
   length: 60 * 60 // 1 hours in seconds
+}, {
+  // 6:20 PM - 10:20 PM
+  // TODO: add real video
+  url: 'https://www.youtube.com/embed/lM02vNMRRB0' + videoOptions,
+  length: 4 * 60 * 60 // 4 hours in seconds
 }]
 
 const audioOptions = '&amp;auto_play=true&amp;show_artwork=false&amp;show_playcount=false&amp;show_user=false&amp;sharing=false&amp;buying=false&amp;download=false';
@@ -60,15 +86,17 @@ const audios = [{
 
 const wormholes = [{
   id: 'wormhole-1',
-  start: 60 * 60, // 1 hour
-  end: 61 * 60
+  start: 18 * 60 * 60, // 4:20pm
+  end: 19 * 60 * 60
 }]
 
 function convertDateToTimestamp(d) {
-  let startHour = 22;
-  let startMinute = 01;
+  // Make sure to offset by UTC - 4 (EDT)
+  let startHour = window.startHour;
+  let startMinute = window.startMinute;
+
   let date = new Date();
-  let hour = date.getUTCHours() - 4; // UTC - 4 is EDT, east coast time
+  let hour = date.getUTCHours();
   let minute = date.getUTCMinutes();
   let second = date.getUTCSeconds();
 
@@ -91,13 +119,54 @@ function convertSecondsToTimer(n) {
   return new Date(n * 1000).toISOString().substr(14, 5)
 }
 
+/**
+ * Get the URL parameters
+ * source: https://css-tricks.com/snippets/javascript/get-url-variables/
+ * @param  {String} url The URL
+ * @return {Object}     The URL parameters
+ */
+var getParams = function (url) {
+  var params = {};
+  var parser = document.createElement('a');
+  parser.href = url;
+  var query = parser.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    params[pair[0]] = decodeURIComponent(pair[1]);
+  }
+  return params;
+};
+
 window.ready(function() {
+  window.startHour = 2;
+  window.startMinute = 20;
+
+  let params = getParams(window.location.href);
+  let date = new Date();
+  if (params.startHour) {
+    let hour = date.getUTCHours();
+    window.startHour = hour - parseInt(params.startHour) - 2;
+    if (window.startHour > 24) window.startHour -= 24;
+    else if (window.startHour < 0) window.startHour = 24 + window.startHour;
+
+    console.log(window.startHour);
+  }
+  if (params.startMinute) {
+    let minute = date.getUTCMinutes();
+    window.startMinute = minute - parseInt(params.startMinute) + 20;
+    if (window.startMinute > 59) window.startMinute -= 59;
+    else if (window.startMinute < 0) window.startMinute = 60 + window.startMinute;
+  }
   console.log("INIT");
   const checkAndChangeVideo = () => {
     let date = new Date();
     let seconds = convertDateToTimestamp(date);
+
+      console.log(seconds);
     let i = 0
     for (i = 0; i < videos.length; i++) {
+      console.log(seconds, videos[i].length);
       if (seconds > videos[i].length) {
         seconds -= videos[i].length;
       } else {
@@ -152,7 +221,8 @@ window.ready(function() {
   // Title dropdown
   let titleChanger = document.getElementById('title-changer');
   titleChanger.addEventListener('click', (e) => {
-    if (titleChanger.classList.contains('active')) {
+    if (titleChanger.classList.contains('active') &&
+        e.target.tagName === 'LI') {
       let pastActive = titleChanger.querySelector('li.active');
       pastActive.classList.remove('active');
       e.target.classList.add('active');
